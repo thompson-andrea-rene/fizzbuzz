c3f5ee6153fba717828544c044605084ed435431
Andrea Thompson <thompson.andrea.rene@gmail.com>
2024-10-27 23:04:32 -0400

    First

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
-# add your code here
+
+for i in range (1,101):
+    if i % 3 == 0:
+        if i % 5 ==0:
+            print ("FizzBuzz")
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
 
