2dc90474f2a866ac48660ff8ca50194b5b8eedd1
Andrea Thompson <thompson.andrea.rene@gmail.com>
2024-11-02 19:17:52 -0400

    Edited

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,8 @@
 
 for i in range (1,101):
-    if i % 3 == 0:
-        if i % 5 ==0:
-            print ("FizzBuzz")
+    if i % 3 == 0 and i % 5 ==0:
+         print ("FizzBuzz")
+    elif i % 3 == 0:
         print("Fizz")
     elif i % 5 == 0:
         print("Buzz")
